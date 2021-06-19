5c7e7611b9e1ed9baf94bcec7ce742a9ce040027
arthur <sousanot.23@gmail.om>
2021-06-18 23:30:45 -0300

    Sobreescrevendo a action de POST

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -8,3 +9,9 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
+
+    def list(self, request, *args, **kwargs):
+        return Response({'teste': 123})   
+
+    def create(self, request, *args, **kwargs):
+        return Response({'Hello': request.data['nome']})     
